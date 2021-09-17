0ea3864e591047ed78dd5af1ec27e518bdfb2a96
Dylan Elliott <dylan.elliott@gmail.com>
2021-09-17 17:04:57 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Blue Pill
+
+Commands to facilitate moving from Luhmann-style IDs (e.g: '45af5') to name-based IDs (e.g: the-meaning-of-life)
+
+Takes the title of the note from the first markdown header within it. Removes all punctuation, replaces spaces with hyphens, converts to lower case.
+
+## Commands:
+
+- Rename current file: Will just rename the currently open file
+- Rename all files: Renames all files
---
+++ src/main.ts
@@ -0,0 +1,88 @@
+import { FileExplorer, Plugin, TFile, App } from 'obsidian';
+import { WorkspaceLeaf, MarkdownView } from 'obsidian';
+
+export default class FileExplorerNoteCount extends Plugin {
+    // fileExplorer?: FileExplorer;
+
+    sanitiseTitle(oldTitle: string): string {
+        return oldTitle
+        .toLowerCase()
+        .replace(/[^\w\s]/gi, '')
+        .trim()
+        .replace(/ /g, '-')
+    }
+
+    isZettelFile(file: TFile): boolean {
+        return /^((?:[0-9]+|[a-z]+)+)\.md$/.exec(file.name) != null;
+    }
+
+    getZettels(): TFile[] {
+        return this.app.vault.getMarkdownFiles().filter((file) => {
+            const ignore = /^[_layouts|templates|scripts]/;
+            return file.path.match(ignore) == null && this.isZettelFile(file);
+        });
+    }
+
+
+    async renameFile(file: TFile) {
+        console.log("Renaming " + file.name)
+
+        let val = await this.app.vault.read(file)
+        const match = /# (.+)\s*/.exec(val);
+        
+        if (match == null) { 
+            console.log("Didn't find a title; Skipping")
+            return 
+        }
+
+        const name = match[1];
+        const sanitised = this.sanitiseTitle(name)
+
+        if (sanitised == file.name) {
+            console.log("Already renamed, skipping")
+        }
+
+        console.log("New name: " + sanitised)
+        await this.app.fileManager.renameFile(file, sanitised + ".md")
+        console.log("Done!")
+    }
+
+    renameCurrentFile() {
+        let view = this.app.workspace.getActiveViewOfType(MarkdownView);
+        if (view == null) { return }
+            let file = view.file;
+        this.renameFile(file)
+    }
+
+    async renameAllFiles() {
+        for (const file of this.getZettels()) {
+            await this.renameFile(file)
+        }
+    }
+
+    onload() {
+        console.log('loading RENAMER');
+
+        this.addCommand({
+            id: "rename-current-file",
+            name: "Rename Current File to Markdown Header",
+            callback: () => {
+                this.renameCurrentFile()
+            },
+        });
+
+        this.addCommand({
+            id: "rename-all-files",
+            name: "Rename All Files to their Markdown Headers",
+            callback: () => {
+                this.renameAllFiles()
+            },
+        });
+
+    }
+
+    onunload() {
+        console.log('unloading RENAME');
+    }
+
+}
